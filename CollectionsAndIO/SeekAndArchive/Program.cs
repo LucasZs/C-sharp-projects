41777d0a3f6a582f882cb0fc2858d2bbfa9b580d
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-04-18 07:43:30 +0200

    some debugging and refactoring

--- CollectionsAndIO/SeekAndArchive/Program.cs
+++ CollectionsAndIO/SeekAndArchive/Program.cs
@@ -7,7 +7,7 @@ namespace SeekAndArchive
 {
     class Program
     {
-        static List<FileInfo> FoundFiles;
+        static List<FileInfo> foundFiles;
         static List<FileSystemWatcher> watchers;
         static List<DirectoryInfo> archiveDirs;
 
@@ -15,7 +15,7 @@ namespace SeekAndArchive
         {
             string fileName = args[0];
             string directoryName = args[1];
-            FoundFiles = new List<FileInfo>();
+            foundFiles = new List<FileInfo>();
 
             DirectoryInfo rootDir = new DirectoryInfo(directoryName);
             if (!rootDir.Exists)
@@ -24,25 +24,33 @@ namespace SeekAndArchive
                 return;
             }
 
-            RecursiveSearch(FoundFiles, fileName, rootDir);
-            Console.WriteLine("Found {0} files.", FoundFiles.Count);
-            foreach (FileInfo fil in FoundFiles)
+            RecursiveSearch(foundFiles, fileName, rootDir);
+            Console.WriteLine("Found {0} files.", foundFiles.Count);
+            foreach (FileInfo fil in foundFiles)
             {
                 Console.WriteLine("{0}", fil.FullName);
             }
-
-            Console.ReadKey();
 
             watchers = new List<FileSystemWatcher>();
 
             archiveDirs = new List<DirectoryInfo>();
 
-            for (int i = 0; i < FoundFiles.Count; i++)
+            for (int i = 0; i < foundFiles.Count; i++)
             {
                 archiveDirs.Add(Directory.CreateDirectory("archive" +
                 i.ToString()));
             }
 
+            foreach (FileInfo fil in foundFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
+            }
+
+            Console.ReadKey();
         }
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
@@ -66,7 +74,7 @@ namespace SeekAndArchive
             FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
             int index = watchers.IndexOf(senderWatcher, 0);
 
-            ArchiveFile(archiveDirs[index], FoundFiles[index]);
+            ArchiveFile(archiveDirs[index], foundFiles[index]);
         }
 
 
@@ -75,19 +83,20 @@ namespace SeekAndArchive
             FileStream input = fileToArchive.OpenRead();
             FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
 
-            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            GZipStream compressor = new GZipStream(output, CompressionMode.Compress);
 
             int b = input.ReadByte();
 
             while (b != -1)
             {
-                Compressor.WriteByte((byte)b);
+                compressor.WriteByte((byte)b);
                 b = input.ReadByte();
             }
 
-            Compressor.Close();
+            compressor.Close();
             input.Close();
             output.Close();
         }
+
     }
 }
