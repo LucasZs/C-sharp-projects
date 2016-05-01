52a56a611c914b8363b8f9940fdf92a07509f329
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-05-01 20:15:02 +0200

    TextFileConverter added (Regex exercise 3 solved)

---
+++ Regex and serialzation and multithreading/TextFileConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextFileConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(@"C:\boot.ini");
+                StreamWriter sw = new StreamWriter("boot-utf7.txt", false, Encoding.UTF7);
+
+                sw.WriteLine(sr.ReadToEnd());
+                sw.Close();
+                sr.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
