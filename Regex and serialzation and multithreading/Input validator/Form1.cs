30498f00676d168fc364f6d05a6b35be5dac721d
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-05-01 19:09:02 +0200

    ReformatPhone method is built in Input validator (Regex exercise 2 solved)

--- Regex and serialzation and multithreading/Input validator/Form1.cs
+++ Regex and serialzation and multithreading/Input validator/Form1.cs
@@ -18,6 +18,13 @@ namespace Input_validator
             InitializeComponent();
         }
 
+        static string ReformatPhone(string s)
+        {
+            Match m = Regex.Match(s, @"^\(?(\d{3})\)?[\s\-]?(\d{3})\-?(\d{4})$");
+
+            return String.Format("({0}) {1}-{2}", m.Groups[1], m.Groups[2], m.Groups[3]);
+        }
+
         private void saveButton_Click(object sender, EventArgs e)
         {
             if (!Regex.IsMatch(nameTextBox.Text, @"^([A-Za-z]*\s*)*$"))
@@ -28,6 +35,8 @@ namespace Input_validator
 
             if (!Regex.IsMatch(emailTextBox.Text, @"^([a-zA-Z0-9_\-” [email protected]\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
                 MessageBox.Show("The e-mail address is not valid.");
+            
+            phoneTextBox.Text = ReformatPhone(phoneTextBox.Text);
         }
     }
 }
