efa97f642e6f230915a196ccdd3407d8f664c648
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-05-02 11:08:14 +0200

    input validator debugged

--- Regex and serialzation and multithreading/Input validator/Form1.cs
+++ Regex and serialzation and multithreading/Input validator/Form1.cs
@@ -27,13 +27,13 @@ namespace Input_validator
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-            if (!Regex.IsMatch(nameTextBox.Text, @"^([A-Za-z]*\s*)*$"))
+            if (!Regex.IsMatch(nameTextBox.Text, @"^([A-Za-z]+\s*)+$"))
                 MessageBox.Show("The name is invalid (only alphabetical characters are allowed)");
 
-            if (!Regex.IsMatch(phoneTextBox.Text, @"^((\(\d{3}\)?)|(\d{3}-))?\d{3}-\d{4}$"))
+            if (!Regex.IsMatch(phoneTextBox.Text, @"^((\(\d{3}\))|(\d{3}-))?\d{3}-\d{4}$"))
                 MessageBox.Show("The phone number is not a valid US phone number");
 
-            if (!Regex.IsMatch(emailTextBox.Text, @"^([a-zA-Z0-9_\-” [email protected]\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
+            if (!Regex.IsMatch(emailTextBox.Text, @"^([a-zA-Z0-9_\-”+@”\.]+)@((\[[0-9]{1,3}"+@"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\"+@".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"))
                 MessageBox.Show("The e-mail address is not valid.");
             
             phoneTextBox.Text = ReformatPhone(phoneTextBox.Text);
