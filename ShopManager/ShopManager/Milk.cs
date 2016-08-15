d9bed7c5c281e86eba8e15e15c509021bdd558d4
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-08-15 02:03:50 +0200

    Shop manager - first task added

---
+++ ShopManager/ShopManager/Milk.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManager
+{
+    public class Milk
+    {
+        //variables:
+        int capacity; //ml
+        string company;
+        DateTime warrant;
+        double dripping;
+        long price; //HUF
+
+        //constructor:
+
+        public Milk(int capacity, string company, DateTime warrant, double dripping, long price)
+        {
+            this.capacity = capacity; // liter
+            this.company = company;
+            this.warrant = warrant;
+            this.dripping = dripping;
+            this.price = price; // Ft
+        }
+
+        // getters:
+
+        public int getCapacity()
+        {
+            return capacity;
+        }
+
+        public string getCompany()
+        {
+            return company;
+        }
+
+        public DateTime getWarrant()
+        {
+            return warrant;
+        }
+
+        public double getDripping()
+        {
+            return dripping;
+        }
+
+        public long getPrice()
+        {
+            return price;
+        }
+
+        // other methods:
+        public bool isGood()
+        {
+            return warrant >= DateTime.Now;
+        }
+
+        public string toString()
+        {
+            return "Produced by " + company + " Best before: " + warrant + " Dripping: " + dripping;
+        }
+    }
+
+    public class MilkRunner
+    {
+        static void Main(string[] args)
+        {
+            Milk mizo = new Milk(1000, "Sole-Mizo ZRt.", new DateTime(2016, 8, 18), 2.8, 200);
+            Milk parmalat = new Milk(1500, "Lactalis Hungária Kft.", new DateTime(2016, 8, 18), 1.5, 259);
+            Milk milli = new Milk(330, "FrieslandCampina Hungária ZRt.", new DateTime(2016, 9, 15), 3.5, 179);
+
+            Console.WriteLine("The warrant date of our Mizo milk is " + mizo.getWarrant());
+            Console.WriteLine("The company behind Parmalat milk is " + parmalat.getCompany());
+            Console.WriteLine("The capacity of our Milli milk is " + milli.getCapacity() + " litre.");
+            Console.WriteLine("Milli milk is " + (milli.isGood() ? "good" : "bad"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ShopManager/ShopManagerTests/MilkTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopManager.Tests
+{
+    [TestClass()]
+    public class MilkTests
+    {
+        [TestMethod()]
+        public void getCapacityTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual(1000, m.getCapacity());
+        }
+
+        [TestMethod()]
+        public void getCompanyTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual("Tej Rt.", m.getCompany());
+        }
+
+        [TestMethod()]
+        public void getWarrantTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual((new DateTime(2016, 9, 1)), m.getWarrant());
+        }
+
+        [TestMethod()]
+        public void getDrippingTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual(2.8, m.getDripping());
+        }
+
+        [TestMethod()]
+        public void getPriceTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual(200, m.getPrice());
+        }
+
+        [TestMethod()]
+        public void isGoodTest()
+        {
+            Milk m = new Milk(1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8, 200);
+            Assert.AreEqual(true, m.isGood());
+        }
+     }
+}
