bf121f418b4b739c94e71e8bcaa999d8202cb43b
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-09-08 14:58:57 +0200

    testfiles fixed and completed

---
+++ ShopManager/NUnitTests/HalflongLifeMilkNTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ShopManager.NTests
+{
+    [TestFixture]
+    public class HalfongLifeMilkTests
+    {
+        [Test]
+        public void GetBarcodeTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual(678524, m.GetBarcode());
+        }
+
+        [Test]
+        public void GetCapacityTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual(1000, m.GetCapacity());
+        }
+
+        [Test]
+        public void GetCompanyTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual("Tej Rt.", m.GetCompany());
+        }
+
+        [Test]
+        public void GetWarrantTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual((new DateTime(2016, 9, 1)), m.GetWarrant());
+        }
+
+        [Test]
+        public void GetDrippingTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual(2.8, m.GetDripping());
+        }
+
+        [Test]
+        public void IsGoodTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            Assert.AreEqual(false, m.IsGood());
+        }
+
+        [Test]
+        public void ToStringTest()
+        {
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            // Be cHalflongLifeMilkareful: using '.' or ',' in double/float depends on environmental variables:
+            Assert.AreEqual("Produced by: Tej Rt. Best before: 2016.09.01. 0:00:00 Dripping: 2,8", m.ToString());
+        }
+    }
+}
--- ShopManager/ShopManagerTests/HalflongLifeMilkTests.cs
+++ ShopManager/ShopManagerTests/HalflongLifeMilkTests.cs
@@ -9,55 +9,55 @@ using System.Threading.Tasks;
 namespace ShopManager.Tests
 {
     [TestClass()]
-    public class LonglifeMilkTests
+    public class HalfongLifeMilkTests
     {
         [TestMethod()]
         public void GetBarcodeTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual(678524, m.GetBarcode());
         }
 
         [TestMethod()]
         public void GetCapacityTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual(1000, m.GetCapacity());
         }
 
         [TestMethod()]
         public void GetCompanyTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual("Tej Rt.", m.GetCompany());
         }
 
         [TestMethod()]
         public void GetWarrantTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual((new DateTime(2016, 9, 1)), m.GetWarrant());
         }
 
         [TestMethod()]
         public void GetDrippingTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual(2.8, m.GetDripping());
         }
 
         [TestMethod()]
         public void IsGoodTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
             Assert.AreEqual(false, m.IsGood());
         }
 
         [TestMethod()]
         public void ToStringTest()
         {
-            LonglifeMilk m = new LonglifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
-            // Be careful: using '.' or ',' in double/float depends on environmental variables:
+            HalflongLifeMilk m = new HalflongLifeMilk(678524, 1000, "Tej Rt.", new DateTime(2016, 9, 1), 2.8);
+            // Be cHalflongLifeMilkareful: using '.' or ',' in double/float depends on environmental variables:
             Assert.AreEqual("Produced by: Tej Rt. Best before: 2016.09.01. 0:00:00 Dripping: 2,8", m.ToString());
         }
     }
