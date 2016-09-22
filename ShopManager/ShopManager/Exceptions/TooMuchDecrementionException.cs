f3e357bb10d35b441f099ab51b9fc6e38e02135c
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-09-22 11:32:47 +0200

    all unused 'using' are deleted

--- ShopManager/ShopManager/Exceptions/TooMuchDecrementionException.cs
+++ ShopManager/ShopManager/Exceptions/TooMuchDecrementionException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopManager.Exceptions
+﻿namespace ShopManager.Exceptions
 {
     public class TooMuchDecrementionException : ShopException
     {
