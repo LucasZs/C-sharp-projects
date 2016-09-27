fafd0e4b30ef12f07f506682c3a82f4e8aec446b
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-09-27 11:01:30 +0200

    Soap class added

---
+++ ShopManager/ShopManager/Soap.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopManager
+{
+    public class Soap : Ware
+    {
+        public readonly char WASHEFFECT_A = 'A';
+        public readonly char WASHEFFECT_B = 'B';
+        readonly char washEffect;
+
+        public Soap(long barcode, string company, char washEffect) : base(barcode, company)
+        {
+            this.washEffect = washEffect;
+        }
+    }
+}
